c19b75aecc3a53da07b56e56af561d9057e9498e
Vanaro <vanarothorn01@gmail.com>
2021-02-01 15:21:42 -0600

    Created router for dishes

--- index.js
+++ index.js
@@ -3,37 +3,13 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/dishes', (req, res, next) => {
-  res.end('Will send all the dishes to you');
-});
-
-app.post('/dishes', (req, res, next) => {
-  res.end(
-    'Will add the dish: ' +
-      req.body.name +
-      ' with details: ' +
-      req.body.description
-  );
-});
-
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-  res.end('Deleting all the dieshes!');
-});
+app.use('/dishes', dishRouter);
 
 app.get('/dishes/:dishId', (req, res, next) => {
   res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
